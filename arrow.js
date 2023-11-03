ea65a42f59dbb3148a02b4100590017786987071
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-11-04 01:48:50 +0600

    first commit

---
+++ arrow.js
@@ -0,0 +1,47 @@
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(2,3));
+
+// const add = (a,b) => {
+//     return a+b
+// }
+// console.log(add(2,3));
+
+// const add = (a,b) =>{
+//     return a+b
+// }
+// console.log(add(5,5));
+// function show(){
+//     console.log('Hello World');
+// }
+// show()
+
+// function printNumber(x){
+//     console.log('Value is' , x);
+// }
+// printNumber(49)
+
+// const add = (a,b) => a+b;
+// console.log(add(5,2));
+
+// const show = () => {
+//     console.log('Hello');
+// }
+// show()
+
+// const num = x => console.log(x);
+// num(4)
+
+const add = (a,b) => {
+    return a+b;
+}
+console.log(add(5,5));
+
+const sum = () => {
+    console.log('Hello');
+}
+sum()
+
+const num = x => console.log(x);
+num(5)
